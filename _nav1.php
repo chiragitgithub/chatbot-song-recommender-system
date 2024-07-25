eed1c2b47675aba27ed1eb29153743a4fb294ff7
chirag Gujar <117736144+chiragitgithub@users.noreply.github.com>
2024-07-25 22:54:51 +0530

    Add files via upload

---
+++ _nav1.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background-color:#333;"> 
+  <!-- <a class="navbar-brand" >Tickit booking</a> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  
+
+
+        <a class="nav-link" href="musicH.php" style="color:white;"> M U S I C - P L A Y E R </a>
+      
+        
+      
+        
+
+        
+        
+      
+     
+     
+       
+    </form>
+  </div>
+</nav>
